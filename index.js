4df7972d367629cffeacf099497cd767bb6c1e6e
Sergio Nezhigay <abc1971h@gmail.com>
2023-01-29 13:01:03 +0200

    yes!

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
 const people = [
   {name: "Ivan", id: 1, parentId: null}, 
   {name: "Sergio", id: 4, parentId: null},  
-
   {name: "Helen", id: 2, parentId: 1},
   {name: "Danya", id: 8, parentId: 4},
-
   {name: "Pavel", id: 3, parentId: 2},
   {name: "Olya", id: 5, parentId: 2},
   {name: "Nana", id: 6, parentId: 5},
   {name: "Ira", id: 7, parentId: 6},
-
   {name: "Alex", id: 9, parentId: 1},
 ];
 
-const res=[];
-console.log("start6");
-people.forEach(man => {
-
-  if (! man.parentId) {
-    res.push({...man, children: []});
-  }  
-  else {
-      parent = findParentInRes(res, man.parentId); // ??
-      parent.children.push({...man, children: []});
+function main () {
+  const result=[];
+  people.forEach(person => {
+    if (! person.parentId) {
+      res.push({...person, children: []});
+    }  
+    else {
+      children = findParentInArray(res, person.parentId, person.name, person); // ??
+      if (children) 
+        children.push({...person, children: []});
     }
-});
+  });
+  console.log("result=", result);
+}
 
 
-function findParentInRes(arrayOfParents, parentId) {
-console.log("🚀 ~ file: index.js:33 ~ findParentInRes ~ parentId", parentId)
-
-  for (man of arrayOfParents) {
-    console.log("🚀 ~ file: index.js:42 ~ findParentInRes ~ man", man)
-    if (man.id === parentId) {
-      console.log("found result, man=", man);
-      return man;
+function findParentInArray(arrayOfParents, parentId, name, person) {
+  console.log("run findParentInArray for id/name ", parentId, name);
+  arrayOfParents.forEach (mayBeFather => {
+    if (mayBeFather.id === parentId) {
+      console.log("found mayBeFather, person=", mayBeFather);
+      console.log("for sun/daughter with id=", parentId);
+      mayBeFather.children.push({...person, children: []});
+      return true;
     }
     else
-      findParentInRes(man.children, parentId); 
-  }
+      findParentInArray(mayBeFather.children, parentId, name, person); 
+  });
 }
